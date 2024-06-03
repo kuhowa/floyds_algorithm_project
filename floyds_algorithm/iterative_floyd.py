77ad1f01b9d54b08cedbc51c38bad2b7f9d07d63
kuhowa <153405357+kuhowa@users.noreply.github.com>
2024-06-03 23:44:40 +0800

    Update iterative_floyd.py

--- floyds_algorithm/iterative_floyd.py
+++ floyds_algorithm/iterative_floyd.py
@@ -1,8 +1,14 @@
+# floyds_algorithm/iterative_floyd.py
+
 def floyd(distance):
-      for intermediate in range(len(distance)):
+    """
+    A simple implementation of Floyd's algorithm
+    """
+    for intermediate in range(len(distance)):
         for start_node in range(len(distance)):
             for end_node in range(len(distance)):
                 distance[start_node][end_node] = min(
                     distance[start_node][end_node],
                     distance[start_node][intermediate] + distance[intermediate][end_node]
                 )
+    return distance
